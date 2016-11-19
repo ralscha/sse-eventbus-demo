59054ea25815c5a081cd7ed7b6591c006df119d0
Ralph Schaer <ralphschaer@gmail.com>
2016-11-19 05:44:56 +0100

    Add missing @EnableSseEventBus
    Is not neccessary here because demo application and library sit in the
    same package and Spring Boot finds the @Configuration class in the
    eventbus library automatically.

--- src/main/java/ch/rasc/Application.java
+++ src/main/java/ch/rasc/Application.java
@@ -4,8 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import ch.rasc.sse.eventbus.config.EnableSseEventBus;
+
 @SpringBootApplication
 @EnableScheduling
+@EnableSseEventBus
 public class Application {
 
 	public static void main(String[] args) {
