ce18c402ca692eb5e5c4d66da7a1496fd6146c3a
Ralph Schaer <ralphschaer@gmail.com>
2018-01-30 16:13:05 +0100

    Example with polyfill.io

---
+++ client2/src/main.js
@@ -0,0 +1,25 @@
+import App from './app';
+
+if (browserSupportsAllFeatures()) {
+    new App().start();
+} else {
+    loadScript('https://cdn.polyfill.io/v2/polyfill.min.js?features=EventSource,Symbol,Array.prototype.@@iterator,Array.prototype.entries', function () {
+        new App().start();
+    });
+}
+
+function browserSupportsAllFeatures() {
+    return window.EventSource && window.Symbol && Array.prototype.entries;
+}
+
+function loadScript(src, done) {
+    var js = document.createElement('script');
+    js.src = src;
+    js.onload = function () {
+        done();
+    };
+    js.onerror = function () {
+        done(new Error('Failed to load script ' + src));
+    };
+    document.head.appendChild(js);
+}
